4eb8f5391c9c536c2a58f99418d2f144d65ae333
huskar435 <godkraken@tutamail.com>
2024-10-22 23:43:56 +0300

    firstcommit

--- geoprogress.py
+++ geoprogress.py
@@ -1,14 +1,16 @@
-input_data = open("input.txt","r")
+input_data = open('input.txt', 'r')
 data = input_data.read()
+
 data = data.split()
-a = 0
-b = int(data[0])
+
+b1 = int(data[0])
 q = int(data[1])
 n = int(data[2])
-for a in range(1,1000):
-    if ((b *(1 - (q ** n))) / ( 1- q)) == a:
-     str(a)
-output_data = open("output.txt","w")
-output_data.write(str(a))
+
+s = int(b1*(1-q**n)/(1-q))
+
+output = open('output.txt', 'w')
+output.write(str(s))
+
 input_data.close()
-output_data.close()   
+output.close()
--- operations.py
+++
@@ -1,6 +0,0 @@
-input_data = open("input.txt","r")
-data = input_data.read()
-data = data.split()
-a = int(data[0])
-for i in range(1,100000):
-    a
